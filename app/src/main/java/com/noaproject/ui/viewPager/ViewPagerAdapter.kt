1bccc0553d4802a2f15a554db8f449635701c5c6
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-11-27 23:44:15 +0100

    Created ViewPage Adapter

---
+++ app/src/main/java/com/noaproject/ui/viewPager/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.noaproject.ui.viewPager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class ViewPagerAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm){
+
+    private val mFrgmentList = ArrayList<Fragment>()
+    private val mFrgmentTitleList = ArrayList<String>()
+
+
+    override fun getCount() = mFrgmentList.size
+
+    override fun getItem(position: Int) = mFrgmentList[position]
+
+    fun getTitle(position: Int) = mFrgmentTitleList[position]
+
+    override fun getPageTitle(position: Int) = mFrgmentTitleList[position]
+
+    fun addFragment(fragment: Fragment, title:String){
+        mFrgmentList.add(fragment)
+        mFrgmentTitleList.add(title)
+    }
+    fun clearFragment(){
+        mFrgmentList.clear()
+        mFrgmentTitleList.clear()
+    }
+}
